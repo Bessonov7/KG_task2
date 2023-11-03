31d568ed5826e33ada7b77e3fb26f8778b441595
Ylyas <ki48bes2004@gmail.com>
2023-11-03 15:58:10 +0300

    added Vec2 and Line objects

---
+++ src/Geometry/Line.java
@@ -0,0 +1,11 @@
+package Geometry;
+
+public class Line {
+    public final Vec2 start;
+    public final Vec2 end;
+
+    public Line(Vec2 start, Vec2 end) {
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ src/Geometry/Vec2.java
@@ -0,0 +1,10 @@
+package Geometry;
+
+public class Vec2 {
+    public Integer x;
+    public Integer y;
+    public Vec2(Integer x, Integer y){
+        this.x = x;
+        this.y = y;
+    }
+}
